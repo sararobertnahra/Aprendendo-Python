606ea675778320e1a0d5e56789f1ec426a435305
Sara Nahra <sara.nahra@fatec.sp.gov.br>
2023-07-30 19:46:47 +0000

    operacoesbasicas

---
+++ stringformatting.py
@@ -0,0 +1,31 @@
+nome = "Sara"
+idade = 30
+print("A %s tem %d anos." %(nome, idade))
+
+
+astring = "HeLlo world!"
+print(astring.count("l"))
+
+astring = "Hello world!"
+print(astring[3:7]) #Retorna lo w
+
+astring = "Hello world!"
+print(astring[3:]) #Retorna lo world!
+
+astring = "Hello world!"
+print(astring[:7]) #Retorna Hello w
+
+astring = "Hello world!"
+print(astring[3:7:2]) #Retorna l
+#[começar:parar:passo ou start:stop:step]
+
+astring = "Hello world!"
+print(astring[::-1]) #Retorna !dlrow olleH
+
+astring = "Hello world!"
+print(astring.upper())
+print(astring.lower())
+
+astring = "Hello world!"
+afewwords = astring.split(" ")
+
