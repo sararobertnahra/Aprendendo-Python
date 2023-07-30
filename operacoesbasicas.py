5f17ded3d28e0f665ad8ab55e6b048b9608ed5eb
Sara Nahra <sara.nahra@fatec.sp.gov.br>
2023-07-30 15:13:21 +0000

    variables-types

---
+++ operacoesbasicas.py
@@ -0,0 +1,13 @@
+restodadivisao = 28 % 3
+print(restodadivisao)
+
+
+numero = 4 + 6 * 7 - 5 / 5
+print(numero)
+
+quadrado = 3 ** 2
+cubo = 7 ** 3
+print(quadrado, cubo)
+
+repetindopalavras = "Sara" * 10
+print(repetindopalavras)
---
+++ variables-types.py
@@ -0,0 +1,37 @@
+print ("Hello!")
+
+
+numerointeiro = 7
+print (numerointeiro)
+
+decimal = 7.0 
+print(decimal)
+decimal = float(7)
+print(decimal)
+
+minhastring = 'Oi'
+print (minhastring)
+minhastring = "Olá"
+print (minhastring)
+
+um = 1
+dois = 2
+tres = um + dois
+print (tres)
+
+ola = "Olá"
+mundo = "mundo!"
+olamundo = ola + " " + mundo
+print(olamundo)
+
+lista = []
+lista.append(5)
+lista.append(10)
+lista.append(20)
+print(lista[0])
+print(lista[1])
+print(lista[3])
+
+for x in lista:
+    print(x)
+
